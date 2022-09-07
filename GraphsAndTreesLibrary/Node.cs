162a828687071c3800144e69ae145a04a1de15b6
jered <me@jered.dev>
2022-09-06 21:48:38 -0600

    Create Node class.
    Create Node class with AddChild and DepthFirstSearch methods. The DFS works for  a limited situation, just adding traversed noded to a list. I'll need more robust code to search a graph for a specific node, for example.

---
+++ GraphsAndTreesLibrary/Node.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphsAndTreesLibrary
+{
+    public class Node
+    {
+        public string name;
+        public List<Node> children = new List<Node>();
+
+        public Node(string name)
+        {
+            this.name = name;
+        }   
+        
+        public Node AddChild(string name)
+        {
+            Node child = new Node(name);
+            children.Add(child);
+            return this;
+        }
+
+        public List<string> DepthFirstSearch(List<string> array)
+        // Time: O(v + e) where v is the # of vertics and e the edges
+        // Space: O(v)
+        {
+            array.Add(this.name);
+            for (int i = 0; i < children.Count; i++)
+            {
+                children[i].DepthFirstSearch(array);
+            }
+            return array;
+        }
+    }
+}
